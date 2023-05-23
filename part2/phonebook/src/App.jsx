47cbe4e1284097501f81b42ff4c553e89cc3ff48
Otavio Coelho <otavio.gmcoelho@gmail.com>
2023-05-23 18:20:49 -0300

    Started using Webstorm

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,62 +1,68 @@
-import { useState, useEffect } from 'react'
+import {useState, useEffect} from 'react'
 import axios from 'axios'
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Person from './components/Person';
 
-
 const App = () => {
 
-  useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
-      })
-  }, [])
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                setPersons(response.data)
+            })
+    }, [])
 
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [newFilter, setNewFilter] = useState('')
+    const [persons, setPersons] = useState([])
+    const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
+    const [newFilter, setNewFilter] = useState('')
 
-  const addName = (event) => {
-    event.preventDefault()
-    const nameObject = { name: newName, number: newNumber }
-    const nameArray = persons.map(person => person.name)
-    nameArray.includes(nameObject.name)
-      ? (setNewName(''), alert(`${nameObject.name} already in the list!`))
-      : (setPersons(persons.concat(nameObject)), setNewName(''), setNewNumber(''))
-  }
+    const addPerson = (event) => {
+        event.preventDefault()
+        const personObject = {name: newName, number: newNumber}
+        const personsArray = persons.map(person => person.name)
+        if (personsArray.includes(personObject.name)) {
+            console.log("if works");
+            alert(`${personObject.name} already in the list!`);
+        } else {
+            axios
+                .post('http://localhost:3001/persons', personObject)
+                .then(response => {
+                    setPersons(persons.concat(response.data));
+                    setNewName('');
+                    setNewNumber('');
+                })
+        }
 
-  function handleNameChange(event) {
-    console.log(event.target.value);
-    setNewName(event.target.value);
-  }
+    }
 
-  const handleNumberChange = (event) => {
-    console.log(event.target.value)
-    setNewNumber(event.target.value);
-  }
+    function handleNameChange(event) {
+        console.log(event.target.value);
+        setNewName(event.target.value);
+    }
 
-  const handleFilterChange = (e) => {
-    setNewFilter(e.target.value)
-  }
+    const handleNumberChange = (event) => {
+        console.log(event.target.value)
+        setNewNumber(event.target.value);
+    }
 
-  const filteredPersons = newFilter === ''
-    ? persons
-    : persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
+    const handleFilterChange = (e) => {
+        setNewFilter(e.target.value)
+    }
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <Filter value={newFilter} onchange={handleFilterChange} />
-      <h3>add new</h3>
-      <PersonForm addName={addName} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
-      <h3>Numbers</h3>
-      <Person persons={filteredPersons} />
-    </div>
-  )
+    const filteredPersons = newFilter === '' ? persons : persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
+
+    return (<div>
+        <h2>Phonebook</h2>
+        <Filter value={newFilter} onchange={handleFilterChange}/>
+        <h3>add new</h3>
+        <PersonForm addPerson={addPerson} handleNameChange={handleNameChange}
+                    handleNumberChange={handleNumberChange}/>
+        <h3>Numbers</h3>
+        <Person persons={filteredPersons}/>
+    </div>)
 }
 
 export default App
